7e1b5c95a756a7d413bca13f01269661f801c865
Eduardo Skrock <eduardoskrock.dev@gmail.com>
2025-04-27 21:07:25 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,6 +4,9 @@ import asyncio
 import pytest_asyncio
 import random
 from unittest.mock import patch
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 import main
 
 @pytest.mark.asyncio
